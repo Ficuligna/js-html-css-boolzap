78afb697887fe5a821c90aa618319081cd3e7403
Ficuligna <marcorandazzo051291@gmail.com>
2020-04-07 18:10:23 +0200

    aggiunto orario dainamicalli

--- javascript/main.js
+++ javascript/main.js
@@ -7,9 +7,15 @@ $(".sendMessage input").keyup(function(){
   }
 })
 $("#SendOrRec").click(function(){
+  var time = new Date();
+  var minuti = time.getMinutes();
+  if (minuti < 10) {
+    minuti = "0" + minuti
+  }
+  var messageTime = time.getHours() + ":" + minuti
   var newMessage = $(".sendMessage input").val();
   if ($(".sendMessage input").val() != "") {
-    $(".chatActive").append('<div class="containermessage userMessage"><div class="message"><p>' + newMessage + '</p></div></div>');
+    $(".chatActive").append('<div class="containermessage userMessage"><div class="message"><p>' + newMessage + '</p><p>'+ messageTime + '</p></div></div>');
     $("#SendOrRec").html('<i class="fa fa-microphone" aria-hidden="true"></i>')
     newMessage = "";
     $(".sendMessage input").val("");
@@ -18,9 +24,15 @@ $("#SendOrRec").click(function(){
 
 })
 $(".sendMessage input").keypress(function(x){
+  var time = new Date();
+  var minuti = time.getMinutes();
+  if (minuti < 10) {
+    minuti = "0" + minuti
+  }
+  var messageTime = time.getHours() + ":" + minuti
   var newMessage = $(".sendMessage input").val();
   if ($(".sendMessage input").val() != "" && x.which == 13) {
-    $(".chatActive").append('<div class="containermessage userMessage"><div class="message"><p>' + newMessage + '</p></div></div>');
+    $(".chatActive").append('<div class="containermessage userMessage"><div class="message"><p>' + newMessage + '</p><p>'+ messageTime + '</p></div></div>');
     newMessage = "";
     $(".sendMessage input").val("");
   };
